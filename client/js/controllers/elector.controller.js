49a5ba14914c1b982b941ab504ce8c8244f1faca
Mark Malazarte <mark@markmalazarte.com>
2017-01-15 15:46:23 -0500

    Updated markup on Electors Page form. Styling updates.

--- client/js/controllers/elector.controller.js
+++ client/js/controllers/elector.controller.js
@@ -1,8 +1,51 @@
 angular.module('dncElection')
-.controller('ElectorCtrl', function($scope, $state) {
+.controller('ElectorCtrl', function($scope, ElectorService) {
+	console.log('hi hi')
+	$scope.formFilled = false;
+	$scope.messageTypes = false;
+	$scope.pickedEmail = false;
+	$scope.pickedText = false;
+	$scope.pickedPostcard = false;
+	$scope.emailSent = false;
+	$scope.chooseElector = false;
+				
 
 	$scope.get_electors = function(demo) {
-		$state.go('electorResults', {user: demo});
-	};
-	
+		ElectorService.getElectors(demo).then(function(electors) {
+				$scope.formFilled = true;
+				$scope.electors = electors;
+				$scope.chooseElector = true;
+		});
+	}
+
+	$scope.get_state = function(demo) {
+		ElectorService.getState(demo.zip).then(function(electors) {
+				console.log(electors);
+				$scope.formFilled = true;
+		});		
+	}
+	$scope.chooseMessage = function(elector) {
+		$scope.messageTypes = true;
+		$scope.selectedElector = elector;
+	}
+
+	$scope.pickEmail = function() {
+		$scope.pickedEmail = true;
+		$scope.pickedPostcard = false;
+	}
+
+	$scope.pickPostcard = function() {
+		$scope.pickedPostcard = true;
+		$scope.pickedEmail = false;
+	}
+
+	$scope.sendEmail = function(message, elector) {
+		$scope.pickedEmail = false;
+		$scope.emailSent = true;
+	}
+
+	$scope.sendPostcard = function(message, elector) {
+		$scope.pickedPostcard = false;
+		$scope.postcardSent = true;		
+	}
 });
