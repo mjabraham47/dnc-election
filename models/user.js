71b3c47b2394b9c67558676b634976cc84e11ed6
Kamilla <venessiel@gmail.com>
2017-01-23 20:36:15 -0500

    make user emails unique

--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	uniqueValidator = require('mongoose-unique-validator');
+
 
 var UserSchema = new mongoose.Schema({
-    email: String,
+    email: { type: String, required: true, unique: true },
     first_name: String,
     last_name: String,
     gender: String,
@@ -14,6 +16,8 @@ var UserSchema = new mongoose.Schema({
     abroad: Boolean,
     endorsing: {type: mongoose.Schema.Types.ObjectId, ref:'Candidate'}
 });
+
+UserSchema.plugin(uniqueValidator);
 
 
 module.exports = mongoose.model('User', UserSchema);
