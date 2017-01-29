1e4accf169a2d6f0658e2419bb4bdc7452c7a222
Michael <mjabraham@gmail.com>
2017-01-29 14:14:36 -0500

    postcard model adjusted

--- models/postcard.js
+++ models/postcard.js
@@ -4,17 +4,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostcardSchema = new mongoose.Schema({
-	user: [{type: Schema.ObjectId, ref:'User'}],
-	message: String,
-	state: [{type: Schema.ObjectId, ref:'State'}],
-	price: Number,
-	from: {
-            name: String,
-            address_line1: String,
-            address_city: String,
-            address_state: String,
-            address_zip: Number
-            },
+    user: [{ type: Schema.ObjectId, ref: 'User' }],
+    message: String,
+    state: [{ type: Schema.ObjectId, ref: 'State' }],
+    price: Number,
+    from: {
+        name: String,
+        address_line1: String,
+        address_city: String,
+        address_state: String,
+        address_zip: Number
+    }
 });
 
 module.exports = mongoose.model('Postcard', PostcardSchema);
