f7767946e6214f8d93efd004518915756645c4e6
Michael <mjabraham@gmail.com>
2017-01-30 21:33:19 -0500

    changed config

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ var config = {
 	nodeEnv: process.env.NODE_ENV || 'development',
 	database: process.env.MONGODB_URI,
 	database_development: 'mongodb://localhost/dnc-election',
-	paypalUsername: process.env.paypalUsername,
-	paypalPassword: process.env.paypalPassword
+	paypalUsername: process.env.PAYPAL_USERNAME,
+	paypalPassword: process.env.PAYPAL_PASSWORD,
+	paypalSignature: process.env.PAYPAL_SIGNATURE
 };
 
 module.exports = config;
